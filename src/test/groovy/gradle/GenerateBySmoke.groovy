ea118299a12f3dabc12c4d56f0e0fffb92596900
katoquro <katoquro@gmail.com>
2015-08-15 15:39:18 +0300

    add dev target to generate stub json

---
+++ src/test/groovy/gradle/GenerateBySmoke.groovy
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2014-2015 Ainrif <ainrif@outlook.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gradle
+
+import com.ainrif.apiator.core.Apiator
+import com.ainrif.apiator.core.ApiatorConfig
+import com.ainrif.apiator.writer.core.CoreHtmlRenderer
+
+class GenerateBySmoke {
+    static final String jaxrsPackage = 'com.ainrif.apiator.test.model.jaxrs'
+
+    public static void main(String[] args) {
+        def path = "${args[0]}/api.html"
+        def config = new ApiatorConfig(basePackage: jaxrsPackage, renderer: new CoreHtmlRenderer(path))
+
+        new Apiator(config).render()
+    }
+}
---
+++ src/test/groovy/gradle/UpdateStub.groovy
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2014-2015 Ainrif <ainrif@outlook.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gradle
+
+import com.ainrif.apiator.core.Apiator
+import com.ainrif.apiator.core.ApiatorConfig
+import com.ainrif.apiator.writer.core.CoreJsonRenderer
+
+class UpdateStub {
+    static final String jaxrsPackage = 'com.ainrif.apiator.test.model.jaxrs'
+
+    public static void main(String[] args) {
+        def config = new ApiatorConfig(basePackage: jaxrsPackage, renderer: new CoreJsonRenderer())
+
+        new File(args[0]).write(new Apiator(config).render())
+    }
+}
