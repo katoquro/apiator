0ef0497c2def415b8d4b3e2a62f273f66ba4adb9
Andrew Malyhin <katoquro@gmail.com>
2018-04-21 14:49:37 +0300

    update copyright 2018

--- core/src/main/groovy/com/ainrif/apiator/core/spi/Renderer.groovy
+++ core/src/main/groovy/com/ainrif/apiator/core/spi/Renderer.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2017 Ainrif <support@ainrif.com>
+ * Copyright 2014-2018 Ainrif <support@ainrif.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
