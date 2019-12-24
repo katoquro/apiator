770b9c6b64ad45d115680c7bb897c2ac4f7c2d46
zelenkoff <zelenkoff.ev@gmail.com>
2019-12-24 12:02:41 +0300

    Change whats-new icon color after check updates

--- renderer/core-html-renderer/src/main/node/src/components/header/menu/components/Item-list.svelte
+++ renderer/core-html-renderer/src/main/node/src/components/header/menu/components/Item-list.svelte
@@ -1,7 +1,6 @@
 <script>
     import { isModalVisible } from '../../../../stores/single-store';
     import { createEventDispatcher } from 'svelte';
-    import WhatsNewIcon from '../icons/gift-solid.svg';
     import DocumentationIcon from '../icons/book-solid.svg';
     import SourceIcon from '../icons/github-alt-brands.svg';
     import ReportBugIcon from '../icons/bug-solid.svg';
@@ -28,9 +27,25 @@
     <li
         on:click={handleWhatsNewItemClick}
         class="menu__items-item js__menu-item-whats-new {hasUpdates ? 'menu__items-item_notification' : ''}">
-        <i
-            class="menu__items-item-icon"
-            style="background: url({WhatsNewIcon}) no-repeat" />
+        <svg
+            aria-hidden="true"
+            data-prefix="fas"
+            data-icon="gift"
+            class="svg-inline--fa fa-gift fa-w-16 menu__items-item-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512">
+            <path
+                fill={hasUpdates ? '#cd7778' : '#838688'}
+                d="M32 448c0 17.7 14.3 32 32 32h160V320H32v128zm256 32h160c17.7
+                0 32-14.3 32-32V320H288v160zm192-320h-42.1c6.2-12.1 10.1-25.5
+                10.1-40 0-48.5-39.5-88-88-88-41.6 0-68.5 21.3-103
+                68.3-34.5-47-61.4-68.3-103-68.3-48.5 0-88 39.5-88 88 0 14.5 3.8
+                27.9 10.1 40H32c-17.7 0-32 14.3-32 32v80c0 8.8 7.2 16 16
+                16h480c8.8 0 16-7.2 16-16v-80c0-17.7-14.3-32-32-32zm-326.1
+                0c-22.1 0-40-17.9-40-40s17.9-40 40-40c19.9 0 34.6 3.3 86.1
+                80h-86.1zm206.1 0h-86.1c51.4-76.5 65.7-80 86.1-80 22.1 0 40 17.9
+                40 40s-17.9 40-40 40z" />
+        </svg>
         <a
             href="https://github.com/ainrif/apiator/blob/develop/CHANGELOG.md"
             target="_blank"
